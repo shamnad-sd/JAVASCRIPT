10037ef52aedff598bdd1820c64b8006cc18a3b8
SdCode000 <shamnadsdd>
2024-08-20 14:22:01 +0530

    Your commit message describing the changes

--- class/function.js
+++ class/function.js
@@ -59,9 +59,90 @@
 // console.log(car);
 
 // array unshift
-let car =["Benz","BMW","altis"];
-let firstcar = car.shift("");
-console.log(car);
+// let car =["Benz","BMW","altis"];
+// let firstcar = car.shift("");
+// console.log(car);
+
+
+
+
+// ---------------------------------------------rest parameters----------------------------------------------
+
+function add(...values){
+    let sum =0
+    for(let i=0; i<values.length; i++){
+        sum += values[i]
+    }
+    console.log(sum)
+    
+}
+add(10,20,30)
+
+
+// -----------------------------------Destructuring arrys---------------------------------
+
+
+const fruits =["apple","banana","mango"];
+// assign variable
+const [aa,bb,cc]= fruits;
+console.log(aa,bb,cc)
+
+
+// -----------------------------------Destructuring obj---------------------------------
+
+// new way of using an object inside a function
+const student = {
+    name:"shamnad",
+    age:21,
+    gender:"male"
+    }
+    mydetails(student);
+
+    function mydetails({name,age,gender}){
+        const message= 'my name is' + name +'im just'+ age +'old,my gender is'+ gender ;
+        console.log(message)
+    }
+
+
+    // -----------------------------------Speard operators---------------------------------
+
+    const MernBatch =["shamnad","asger","ansaf","habeeb"]
+    const DatascienceBatch =["faadhi","zayd","ashique"]
+
+    const allstudents =[...MernBatch,...DatascienceBatch]
+    console.log(allstudents)
+
+
+    // ---------------------------Javascript Short Circuiting Operators----------------------
+
+    function gfg() {
+        // AND short circuit
+        console.log(false && true)
+        console.log(true && true)
+        // OR short circuit
+        console.log(true || false)
+        console.log(false || true)
+    }
+    gfg();
+    
+    // -----------------------------------JavaScript Nullish Coalescing(??) Operator
+
+    const foo = { 
+        bar: 0 
+    } 
+    
+    const valueBar = foo.bar ?? 42; 
+    const valueBaz = foo.baz ?? 42; 
+    
+    // Value of bar: 0 
+    console.log("Value of bar: ", valueBar); 
+    
+    // Value of bar: 42 
+    console.log("Value of baz: ", valueBaz); 
+
+    
+
+    
 
 
 
