1a6eb3a225d0a9a9fee44ad677b53265bb64a1fb
SdCode000 <shamnad2nk@gmail.com>
2024-09-28 23:00:15 +0530

    JS

---
+++ JavaScript-Classes (Youtube)/Task1/main.js
@@ -0,0 +1,12 @@
+// var name ='shamnad';
+// var hob ='singing';
+
+// // Assuming you have HTML elements with IDs "nameSection" and "hobSection"
+// const nameShow = document.querySelector("#nameSection")
+// nameShow.innerHTML = `My Name Is : ${name}`
+
+// console.log(nameShow);
+
+// const hobShow = document.querySelector("#hobbiesSection")
+// hobShow.innerHTML = `My Hobby Is : ${hob}`
+// console.log(hobShow);
---
+++ JavaScript-Classes (Youtube)/Task2/array.js
@@ -0,0 +1,57 @@
+// this is doing for i already create li tags in html file, then i create array and values , then i passing this array values from html file
+// this is not to right way we can choose forloop
+
+// let cars =["bmw","porshe","Toyoto","Kia","landRover"]
+
+// let carDetails= document.querySelectorAll('li')
+// carDetails[0].innerHTML=cars[1];
+// carDetails[1].innerHTML=cars[3]
+// carDetails[2].innerHTML=cars[2]
+// carDetails[3].innerHTML=cars[0] 
+// carDetails[4].innerHTML=cars[4] 
+// console.log(cars);
+
+
+// this is the other way to passing value but this have not need to html li this have already li
+// let cars =["bmw","porshe","Toyoto","Kia","landRover"];
+
+// for(let Carcollections of cars){
+//     console.log(Carcollections);
+
+// }
+
+
+
+
+
+
+/* METHOD 1 */
+// this way doing for we can create html elements in javascript
+
+// let carcoll = document.getElementById("carsId")
+// let cars =["bmw","porshe","Toyoto","Kia","landRover","Tata"];
+
+
+// this for loop working :first this carcollection pick cars's data's 
+// the we have to create lis tag 
+// the clg that So 
+// carcollections pick cars first bmw then they create one list tag then that console 
+// carcollection pick second porshe then.......
+// and last total four li they having create
+
+
+// for(let Carcollections of cars){
+//     let li =document.createElement('li')
+//     li.innerHTML= Carcollections;
+//     carcoll.appendChild(li);
+//     console.log(li);
+
+// }
+
+/*METHOD 2*/
+// THIS WAY WE ARE USING FUNCTION FOR 
+let carcoll = document.getElementById("carsId")
+let cars =["bmw","porshe","Toyoto","Kia","landRover","Tata"];
+
+
+
